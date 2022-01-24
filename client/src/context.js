83db583f6d074a5b3218128704f3e42bfa58de2a
Sherron <sherron@Sherrons-MacBook-Air.local>
2022-01-24 06:41:15 -0500

    added context to help handle state

---
+++ client/src/context.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+import React, { useState, useContext, useEffect, createContext } from 'react'
+import { useCallback } from 'react'
+import SingleRecipe from './components/SingleRecipe/SingleRecipe'
+
+
+// const apiKey: `${process.env.REACT_APP_API_KEY}`
+// const url = 'https://api.spoonacular.com/recipes/complexSearch?'
+console.log(process.env)
+// const url = 'https://api.spoonacular.com/recipes/complexSearch?&apiKey='
+// const api_key = process.env.REACT_APP_API_KEY
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState('a')
+  const [recipes, setRecipes] = useState([])
+
+  const fetchRecipes = useCallback( async () => {
+    setLoading(true)
+    try {
+      let {data} = await axios.get(`http://localhost:8000/api/recipes/${searchTerm}&number=30`)
+      const individualRecipes = data.results
+      console.log(data.results)
+      if (individualRecipes) 
+      {
+        const newRecipes = individualRecipes.map((individualRecipe) => {
+          const {
+            id,
+            image,
+            imageType,
+            title
+          } = individualRecipe
+
+          return {
+            number: id,
+            picture: image,
+            pictureType: imageType,
+            name: title,
+          }  
+
+        })
+        setRecipes(newRecipes)
+
+      } else {
+        setRecipes([])
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  },[searchTerm])
+
+  useEffect(() => {
+    fetchRecipes()
+  }, [searchTerm,fetchRecipes])
+
+  return (
+    <AppContext.Provider
+      value={{ loading, recipes, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+export { AppContext, AppProvider }
